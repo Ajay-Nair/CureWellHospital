431abb69408cb00fd6bc2c56a231056db460a3bf
BivinCBenny <bivincbenny@gmail.com>
2023-09-22 12:52:13 +0530

    integrated sidebar and created data service

--- DoctorAppFrontend/src/app/components/side-bar/side-bar.component.ts
+++ DoctorAppFrontend/src/app/components/side-bar/side-bar.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.css']
+  styleUrls: ['./side-bar.component.css'],
 })
 export class SideBarComponent {
+  sideMenuToggle() {
+    let sideBar = document.getElementById('sideBar');
+    sideBar?.classList.toggle('active');
 
+    let menuIcon = document.getElementById('menuIcon');
+    menuIcon?.classList.toggle('rotate');
+  }
 }
--- DoctorAppFrontend/src/app/pages/view-doctors/view-doctors.component.ts
+++ DoctorAppFrontend/src/app/pages/view-doctors/view-doctors.component.ts
@@ -5,12 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './view-doctors.component.html',
   styleUrls: ['./view-doctors.component.css'],
 })
-export class ViewDoctorsComponent {
-  sideMenuToggle() {
-    let sideBar = document.getElementById('sideBar');
-    sideBar?.classList.toggle('active');
-
-    let menuIcon = document.getElementById('menuIcon');
-    menuIcon?.classList.toggle('rotate');
-  }
-}
+export class ViewDoctorsComponent {}
